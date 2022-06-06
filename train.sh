37ab3c374869d24ecadae716ca067b6207588f69
123 <123>
2022-06-06 14:10:22 +0800

    update for training

--- train.sh
+++ train.sh
@@ -26,12 +26,12 @@ CUDA_VISIBLE_DEVICES=1 python tools/train_net.py --config-file configs/Stage3/re
 CUDA_VISIBLE_DEVICES=1 python tools/train_net.py --config-file configs/Stage3/resnet2.yml
 CUDA_VISIBLE_DEVICES=1 python tools/train_net.py --config-file configs/Stage3/resnet1.yml
 ### test for training checkpoints
-python tools/test_for_train.py --config-file   logs/resnet101_1_final/config.yam   --submit-file ./submit1.csv     MODEL.WEIGHTS  ./logs/resnet101_1_final/model_final.pth 
-python tools/test_for_train.py --config-file   logs/resnet101_2_final/config.yam   --submit-file ./submit2.csv     MODEL.WEIGHTS  ./logs/resnet101_2_final/model_final.pth 
-python tools/test_for_train.py --config-file   logs/resnext101_2_final/config.yaml   --submit-file ./submit3.csv   MODEL.WEIGHTS  ./logs/resnext101_2_final/model_final.pth 
-python tools/test_for_train.py --config-file   logs/resnext101_3_final/config.yaml   --submit-file ./submit4.csv   MODEL.WEIGHTS  ./logs/resnext101_3_final/model_final.pth 
-python tools/test_for_train.py --config-file   logs/resnext101_2_final/config.yaml   --submit-file ./submit5.csv   MODEL.WEIGHTS  ./logs/resnext101_con_final/model_final.pth 
-python tools/test_for_train.py --config-file   logs/resnext101_con_final/config.yaml  --submit-file ./submit6.csv  MODEL.WEIGHTS   ./logs/resnext101_mean_tri_final/model_final.pth 
-python tools/test_for_train.py --config-file   logs/resnext101_ms_final/config.yaml    --submit-file ./submit7.csv  MODEL.WEIGHTS ./logs/resnext101_ms_final/model_final.pth 
-python tools/test_for_train.py --config-file   logs/resnext101_1_final/config.yaml   --submit-file ./submit8.csv   MODEL.WEIGHTS  ./logs/resnext101_1_final/model_final.pth 
+python tools/test_for_train.py --config-file   logs/resnext101_1_final/config.yaml   --submit-file ./submit1.csv   MODEL.WEIGHTS  ./logs/resnext101_1_final/model_final.pth 
+python tools/test_for_train.py --config-file   logs/resnext101_ms_final/config.yaml    --submit-file ./submit2.csv  MODEL.WEIGHTS ./logs/resnext101_ms_final/model_final.pth 
+python tools/test_for_train.py --config-file   logs/resnext101_con_final/config.yaml  --submit-file ./submit3.csv  MODEL.WEIGHTS   ./logs/resnext101_mean_tri_final/model_final.pth 
+python tools/test_for_train.py --config-file   logs/resnext101_2_final/config.yaml   --submit-file ./submit4.csv   MODEL.WEIGHTS  ./logs/resnext101_con_final/model_final.pth 
+python tools/test_for_train.py --config-file   logs/resnext101_2_final/config.yaml   --submit-file ./submit5.csv   MODEL.WEIGHTS  ./logs/resnext101_2_final/model_final.pth 
+python tools/test_for_train.py --config-file   logs/resnext101_3_final/config.yaml   --submit-file ./submit6.csv   MODEL.WEIGHTS  ./logs/resnext101_3_final/model_final.pth 
+python tools/test_for_train.py --config-file   logs/resnet101_2_final/config.yam   --submit-file ./submit7.csv     MODEL.WEIGHTS  ./logs/resnet101_2_final/model_final.pth 
+python tools/test_for_train.py --config-file   logs/resnet101_1_final/config.yam   --submit-file ./submit8.csv     MODEL.WEIGHTS  ./logs/resnet101_1_final/model_final.pth 
 python tools/ensemble.py
